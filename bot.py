b5924f8c7afed1374430c585f2577be8b5a87896
RiyoDayoo <jeremiasdiaz413@gmail.com>
2026-05-26 02:21:57 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import discord
 from discord.ext import commands
 
@@ -11,18 +12,18 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-# Give role command
 @bot.command()
 @commands.has_permissions(manage_roles=True)
 async def giverole(ctx, member: discord.Member, *, role: discord.Role):
     await member.add_roles(role)
     await ctx.send(f"Gave {role} to {member.mention}")
 
-# Remove role command
 @bot.command()
 @commands.has_permissions(manage_roles=True)
 async def removerole(ctx, member: discord.Member, *, role: discord.Role):
     await member.remove_roles(role)
     await ctx.send(f"Removed {role} from {member.mention}")
 
-bot.run("MTUwODY1MTYyMjM3OTY4Mzg4Mg.G1LtzP.cLN5bSRw-Eg0b-LwdN3jPg-Ba8yyxFtN3uqMZg")
+TOKEN = os.getenv("TOKEN")
+
+bot.run(TOKEN)
